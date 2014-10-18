5d066e731c55634e92964645fdc25778671220ef
faiqus <faiqrazarizvi@gmail.com>
2014-10-18 19:32:29 -0400

    how do i html

---
+++ Server/index.js
@@ -0,0 +1,17 @@
+var express = require('express')
+  , bodyParser = require('body-parser') 
+  , path = require('path')
+  , http = require('http')
+  , ejs = require('ejs')
+  , app = express()
+  , server = http.createServer(app)
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+app.get('/', function (req, res) { 
+  res.render('index') 
+})
+server.listen(5000)
