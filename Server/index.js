d9c101ba49f9f62fc1cf05018191dd78424a224b
faiqus <faiqrazarizvi@gmail.com>
2014-10-19 09:34:38 -0400

    dasfadsf

--- Server/index.js
+++ Server/index.js
@@ -4,30 +4,36 @@ var express = require('express')
   , http = require('http')
   , ejs = require('ejs')
   , net = require('net') //this will be the listening thing
-  , Ready = require('ready-signal')
+  , edison = require('socket.io-client')('http://localhost:8080')
   , app = express()
-  , r1 = new Ready()
   , server = http.createServer(app)
   , io  = require('socket.io')(server)
 
 app.set('views', path.join(__dirname, 'views')) //tell our webserver to look for views in a folder called views
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
 
-//app.get('/', function (req, res) { 
-  //res.render('index', {noOfTabs : 02}) 
-   r1(function () { 
-        console.log('yoooooo')
-    })                
-//})
+app.get('/', function (req, res) { 
+  res.render('index', {noOfTabs : 02}) 
+})
   
-io.on('connection', function (socket) { 
-  r1()
+
+io.on('connection', function (socket) {
+  var obj = {}
+  obj.data1 = [1,2,3,5,6] 
+  obj.data2 = []
+  obj.data3 = [] 
+  obj.data4 = [] 
+  
+  socket.on('train', function (data) { 
+    obj.data1.push(data.data1)
+    obj.data2.push(data.data2) 
+    obj.data3.push(data.data3) 
+    obj.data4.push(data.data4) 
+    socket.emit('event', obj) 
+  }) 
 })
 
 server.listen(8080)
-
-
-//TCP SERVER
