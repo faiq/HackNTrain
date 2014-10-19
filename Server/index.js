3ce9eef67abacf2a3c8e5d64cf53fc2a652a8487
faiqus <faiqrazarizvi@gmail.com>
2014-10-19 05:44:44 -0400

    tcp for no reason

--- Server/index.js
+++ Server/index.js
@@ -3,8 +3,12 @@ var express = require('express')
   , path = require('path')
   , http = require('http')
   , ejs = require('ejs')
+  , net = require('net') //this will be the listening thing
+  , Ready = require('ready-signal')
   , app = express()
+  , r1 = new Ready()
   , server = http.createServer(app)
+  , io  = require('socket.io')(server)
 
 app.set('views', path.join(__dirname, 'views')) //tell our webserver to look for views in a folder called views
 app.set('view engine', 'ejs');
@@ -12,8 +16,18 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
 
-app.get('/', function (req, res) { 
-  res.render('index', {noOfTabs : 02}) 
-}) //
+//app.get('/', function (req, res) { 
+  //res.render('index', {noOfTabs : 02}) 
+   r1(function () { 
+        console.log('yoooooo')
+    })                
+//})
+  
+io.on('connection', function (socket) { 
+  r1()
+})
 
-server.listen(5000)
+server.listen(8080)
+
+
+//TCP SERVER
